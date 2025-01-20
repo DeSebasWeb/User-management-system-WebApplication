b77984efad56ed439f5fc5d6a6f4da371ffbd640
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2025-01-20 18:38:09 -0500

    Updated version of the project along with the visual design in XHTML and functionalities that enable database connection.

---
+++ src/main/java/user_management_web/controlador/IndexControlador.java
@@ -0,0 +1,35 @@
+package user_management_web.controlador;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.faces.view.ViewScoped;
+import lombok.Data;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import user_management_web.modelo.Usuario;
+import user_management_web.servicio.IUsuarioServicio;
+
+import java.util.List;
+
+@Component
+
+@Data
+@ViewScoped
+public class IndexControlador {
+    @Autowired
+    IUsuarioServicio usuarioServicio;
+    private List<Usuario> usuarios;
+
+    private static final Logger logger = LoggerFactory.getLogger(IndexControlador.class);
+
+    @PostConstruct
+    public void constructor(){
+        cargarDatos();
+    }
+
+    public void cargarDatos(){
+        this.usuarios = this.usuarioServicio.mostrarUsuarios();
+        this.usuarios.forEach(usuario -> logger.info(usuario.toString()));
+    }
+}
