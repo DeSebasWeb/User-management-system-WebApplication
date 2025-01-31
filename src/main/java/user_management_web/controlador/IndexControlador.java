5c759b9050bf54379f382e4c296ebf185f84f1ed
DeSebasWeb <121350219+DeSebasWeb@users.noreply.github.com>
2025-01-30 22:26:14 -0500

    i have beeen adding functions but i have to resolve some problems

--- src/main/java/user_management_web/controlador/IndexControlador.java
+++ src/main/java/user_management_web/controlador/IndexControlador.java
@@ -37,5 +37,35 @@ public class IndexControlador {
         this.usuarios.forEach(usuario -> logger.info(usuario.toString()));
     }
 
+    public void crearUsuario(){
 
+        this.usuarioSeleccionado = new Usuario();
+    }
+
+    public void guardarUsuario(){
+        if (this.usuarioSeleccionado.getId() == null){
+            logger.info("El usuario a guardar es: "+ this.usuarioSeleccionado);
+            this.usuarioServicio.guardarUsuario(this.usuarioSeleccionado);
+            this.usuarios.add(this.usuarioSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuario agregado"));
+        }
+        else{
+            this.usuarioServicio.guardarUsuario(this.usuarioSeleccionado);
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuario actualizado"));
+        }
+        PrimeFaces.current().executeScript("PF('usuarioCrearModificar').hide()");
+        PrimeFaces.current().ajax().update("formulario-aparicion-clientes:mensajes","formulario-aparicion-clientes:usuarios-en-linea");
+        this.usuarioSeleccionado = null;
+    }
+
+    public void eliminarUsuario(){
+        if (this.usuarioSeleccionado.getId() != null){
+            logger.info("Usuario a eliminar: "+this.usuarioSeleccionado);
+            this.usuarioServicio.eliminarUsuario(this.usuarioSeleccionado);
+            this.usuarios.remove(this.usuarioSeleccionado);
+            this.usuarioSeleccionado = null;
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Usuario eliminado con exito"));
+            PrimeFaces.current().ajax().update("formulario-aparicion-clientes:mensajes, formulario-aparicion-clientes:usuarios-en-linea");
+        }
+    }
 }
